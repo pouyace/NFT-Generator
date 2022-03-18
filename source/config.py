ee7def76b73913851c03f2a8e27e85079f070c92
pouyace <thepouya.ce@gmail.com>
2022-03-19 01:55:59 +0330

    First Commit

---
+++ source/config.py
@@ -0,0 +1,15 @@
+#System Config
+
+
+
+#User Config
+NFTsCount = 1200
+DragonType = {"Common":0.94, "Mythic":0.05, "Legendary":0.01}
+Colors = {"Amethyst":18.8, "Crystal":18.8, "Emerald":18.8, "Golden":0.05, "Ruby":18.8, "Shiny":0.01, "Turquoise":18.8}
+
+ITEMS = ["Background", "Body", "Hair", "Head", "Horn", "Eyes", "Glasses", "Orb"]
+
+BGProb = 0.2
+
+
+
