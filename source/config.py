025a0e546c0e2ed17ae9b416a55b1e11013bfa96
pouyace <thepouya.ce@gmail.com>
2022-03-20 22:02:36 +0330

    Version 0.0.2

--- source/config.py
+++ source/config.py
@@ -1,15 +1,85 @@
-#System Config
 
+import random
 
-
-#User Config
+# User Config
 NFTsCount = 1200
-DragonType = {"Common":0.94, "Mythic":0.05, "Legendary":0.01}
-Colors = {"Amethyst":18.8, "Crystal":18.8, "Emerald":18.8, "Golden":0.05, "Ruby":18.8, "Shiny":0.01, "Turquoise":18.8}
-
-ITEMS = ["Background", "Body", "Hair", "Head", "Horn", "Eyes", "Glasses", "Orb"]
-
-BGProb = 0.2
+BACKGROUNDCOLORS = ["Blossom-bg", "Dessert-bg", "Island-bg", "Mountain-bg", "Tropical-bg"]
+ITEMSDIR         = "..\\Circle of Light"
 
 
+# System Config
 
+Probability = "Prob"
+COLORS      = "Colors"
+LEGENADRY   = "Legendary"
+MYTHIC      = "Mythic"
+COMMON      = "Common"
+BACKGROUND  = "Background"
+HAIR        = "Hair"
+HORN        = "Horn"
+EYES        = "Eyes"
+GLASSES     = "Glasses"
+ORB         = "Orb"
+TYPE        = "Type"
+RARITY      = "Rarity"
+EYESCOLOR   = "EyesColor"
+NAME        = "Name"
+
+
+DragonType = {LEGENADRY:{Probability:0.01, COLORS:["Shiny"]},
+              MYTHIC:   {Probability:0.05, COLORS:["Golden"]},
+              COMMON:   {Probability:0.94, COLORS:["Amethyst", "Crystal", "Emerald", "Ruby", "Turquoise"]},
+              }
+
+ITEMS = {BACKGROUND:{TYPE: BACKGROUNDCOLORS},
+         HAIR:      {TYPE:["Braid", "Curly", "Layered", "Straight", "Wavy", "Windy"]},
+         HORN:      {TYPE:["Antler", "Argali", "Kudu", "Stag", "Tahr", "Unicorn"]},
+         EYES:      {TYPE:["Almond", "Eyelash", "Hooded", "Monolid", "Round"]},
+         EYESCOLOR: {TYPE:["Heather", "Orchid", "Violet"]},
+         GLASSES:   {TYPE:["Heather", "Orchid", "Violet"]},
+         ORB:       {TYPE:["Electra", "Ice", "Rock", "Sand", "Wind"]}
+         }
+
+# Dragon name = rarity_type_background_hair_horn_eyes_eyescolor_glasses_orb_
+class Dragon:
+    def __init__(self):
+        self.info = {}
+
+
+
+class DragonGenerator:
+    dragons = []
+    def __init__(self):
+        pass
+
+    def generate(self):
+        dragon = Dragon()
+        info = dragon.info
+        info[RARITY], info[TYPE] = self.selectDragonType()
+        info[NAME] = info[RARITY] + '_' + info[TYPE] + '_'
+
+        for i in ITEMS.items():
+            result = self.randomSelect(i[1][TYPE])
+            info[NAME] += result + '_'
+            info[i[0]] = result
+        return dragon
+
+
+    def randomSelect(self, list):
+        return list[random.randint(0, len(list)-1)]
+
+    def selectDragonType(self):
+        step = 0
+        rand = round(random.random(),2)
+        # print("Random Number:", rand)
+        for i in DragonType.items():
+            step += i[1][Probability]
+            if rand <= step:
+                dragonColor = self.randomSelect(i[1][COLORS])
+                return i[0], dragonColor
+
+    def enrollDragon(self, newDragon):
+        if newDragon not in dragons:
+            dragons.append(newDragon)
+            return true
+        return false
--- source/main.py
+++ source/main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+from os import path
+from config import *
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def startOperation():
+    generator = DragonGenerator()
+    counter = 0
+    while counter != 10:
+        dragon = generator.generate()
+        print(dragon.info)
+        counter += 1
+
+
+
+if __name__ == "__main__":
+    startOperation()
+
+
+
+
+
